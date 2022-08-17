4a7d9c790024dd8e02ec2d6a030a162b6abb067b
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2022-08-17 14:48:33 -0300

    Github actions files changes - ArgoCD sync before tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,17 @@ ENV=$3
 APPSYNC="$APPID-$ENV"
 ARGOURL='argocd-server.argocd.svc.cluster.local'
 
-argocd --server $ARGOURL --insecure version --short
+argo --server $ARGOURL --insecure version --short
 sleep 2
 
 echo "TOKENARGO: $TOKENARGO"
 
 echo -e "\033[0;36m ======> APP $APPSYNC \033[0m\n"
-argocd app get "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure --auth-token "$TOKENARGO"
+argo app get "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure --auth-token "$TOKENARGO"
 
 sleep 5
 
 echo -e "\033[0;36m======> SYNC $APPSYNC \033[0m\n"
-argocd app sync "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure --auth-token "$TOKENARGO"
+argo app sync "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure --auth-token "$TOKENARGO"
 
 echo -e "\033[0;36m======> DONE \033[0m\n"
