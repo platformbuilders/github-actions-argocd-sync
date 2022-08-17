3c1b8ebd0f3b95c89dcdaf672a1f48853207feae
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2022-08-17 17:06:32 -0300

    Changes in module

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -l
 
-argo version --short
-
 # ENV
 APPID=$1
 TOKENARGO=$2
@@ -10,11 +8,11 @@ APPSYNC="$APPID-$ENV"
 ARGOURL='argocd-server.argocd.svc.cluster.local'
 
 echo -e "\033[0;36m ======> APP $APPSYNC \033[0m\n"
-argo app get "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure-skip-tls-verify --auth-token "$TOKENARGO"
+argocd app get "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure-skip-tls-verify --auth-token "$TOKENARGO"
 
 sleep 5
 
 echo -e "\033[0;36m======> SYNC $APPSYNC \033[0m\n"
-argo app sync "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure --auth-token "$TOKENARGO"
+argocd app sync "$APPSYNC" --server "$ARGOURL" --grpc-web --insecure --auth-token "$TOKENARGO"
 
 echo -e "\033[0;36m======> DONE \033[0m\n"
